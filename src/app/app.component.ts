3610ab7533fcf14353e7514069f35ac361f13b1e
Cristian Romo <caromo.33@outlook.com>
2017-12-14 15:41:01 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  color = [];
+  colorpicker(){
+	for (let i = 0; i < 9; i++){
+		var randNum = (Math.floor(Math.random()*10) +1);
+		if (randNum === 1){
+			this.color.push('silver');
+		}
+		if (randNum === 2){
+			this.color.push('gray');
+		}
+		if (randNum === 3){
+			this.color.push('black')
+		}
+		if (randNum === 4){
+			this.color.push('Darkgray')
+		}
+		if (randNum ===5){
+			this.color.push('blue')
+		}
+		if (randNum === 6){
+			this.color.push('red')
+		}
+		if (randNum === 7){
+			this.color.push('lightblue')
+		}
+		if (randNum === 8){
+			this.color.push('darkred')
+		}
+		if (randNum === 9){
+			this.color.push('green')
+		}
+	}
+  }
+  ngOnInit(){
+	  this.colorpicker();
+  }
 }
